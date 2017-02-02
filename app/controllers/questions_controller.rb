ce891957dda65b291e3a857a2b813055cb8f25b8
Pavan Sarguru <pavan.sarguru@gmail.com>
2017-02-02 17:09:01 -0600

    routes and views for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 get '/questions' do
   @questions = Question.all
 
@@ -5,17 +7,34 @@ get '/questions' do
 end
 
 get '/questions/new' do
-
+  binding.pry
+  if session[:user]
+    erb :'questions/new'
+  else
+    erb :'_404'
+  end
 end
 
 post '/questions' do
+  binding.pry
 
+  @question = Question.create(params[:question])
 
+  redirect '/questions'
 end
 
 
 get '/questions/:question_id' do
+  @question = Question.find(params[:question_id])
 
+  erb :'questions/show'
+end
+
+put '/questions/:question_id/edit' do
+
+end
+
+get '/questions/:question_id/edit' do
 
 end
 

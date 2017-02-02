f76504f235bc9a50c0594e63181f51d3e3d29d3c
Al Musavi <email": null>
2017-02-02 17:26:18 -0600

    adding vote button still working on it

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,10 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/answers/:id/vote' do
+  post = Answer.find(params[:id])
+  post.votes.create(up_vote: 1)
+  redirect "/posts"
+
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,14 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :votes, as: :voteable
   has_many :comments, as: :commentable
+
+
+  def up_points
+    (votes.select do {|each_vote| votes.up_vote == true}).length
+  end
+
+  def down_points
+    (votes.select do {|each_vote| votes.up_vote == false}).length
+  end
+
 end

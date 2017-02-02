9e3e2e6c88e6704624149a7622945ee700ebb087
Al Musavi <email": null>
2017-02-02 15:06:52 -0600

    login, register, layout page, session

---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,5 @@
+helpers do
+  def current_user
+     @_current ||= User.find_by(id: session[:user])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   has_many :votes
   has_many :comments
   has_many :recipes
+
+  include BCrypt
+  validates :email, :presence => true
+  validates :hashed_password, :presence => true
+  validates :email, :uniqueness => true
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(plain_password)
+    @password = Password.create(plain_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate?(check_password)
+    self.password == check_password
+  end
 end

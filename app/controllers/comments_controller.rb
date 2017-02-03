9af61230af7ace7b709abcff6ce076530589b28a
Joel Williams <joel.b.williams.is@gmail.com>
2017-02-03 15:17:49 -0600

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -37,8 +37,6 @@ post '/questions/:question_id/recipes/:recipe_id/comments' do
     @question = Question.find_by_id(params[:question_id])
     @recipe = Recipe.find_by_id(params[:recipe_id])
     @comment = @recipe.comments.create(params[:comment])
-    p '8'* 50
-    p @comment
     if @comment.save
       redirect "/questions/#{@question.id}"
     else

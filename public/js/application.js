6d0814d14da2ff0a005e352048d874a3b14b406b
Joel Williams <joel.b.williams.is@gmail.com>
2017-02-03 19:02:22 -0600

    Ajax stuck with current img logic

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   upVote();
   downVote();
+  // voteBest();
 });
+
 
 var upVote = function() {
   $('.upvote-form').on('submit', function(e) {
@@ -42,3 +44,20 @@ var downVote = function() {
   })
 }
 
+var voteBest = function(){
+  $('section.show-recipe').on('submit', 'form#choose-winner', function(e){
+    e.preventDefault();
+    var form = $('form#choose-winner');
+    var url = form.attr('action');
+    // var data = {best_answer: true}
+
+    $.ajax({
+      url: url,
+      method: 'PUT'
+    })
+    .done(function(response){
+      $('form#choose-winner').addClass('hidden')
+      $('img#winner-icon').attr('src', response)
+    })
+  })
+}

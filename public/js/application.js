47a9fa6f300319a5e08064b598a1cb31430917aa
Pavan Sarguru <pavansarguru@gmail.com>
2017-02-05 19:27:28 -0600

    check it now. more css

--- public/js/application.js
+++ public/js/application.js
@@ -95,11 +95,6 @@ var voteBest = function(){
       method: 'PUT'
     })
     .done(function(response){
-      // console.log('no refresh here')
-      // console.log(this)
-      // console.log(form)
-      // console.log(response)
-      // debugger
       $(form).before(response)
       $('form#choose-winner').addClass('hidden')
     })
